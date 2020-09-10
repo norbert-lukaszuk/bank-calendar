bbe638b0ad1dc244dcc34d3555cd8756a362417f
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2020-09-10 13:23:39 +0200

    react-router-dom add

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Aux from "../components/wraper";
-
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import AddTransfer from "../containers/AddTransfer/AddTransfer";
 const Layout = (props) => {
   return (
-    <Aux>
-      <div>Toolbar SideDrawer, Backdrop</div>
-      {props.children}
-    </Aux>
+    <BrowserRouter>
+      <Aux>
+        <div>
+          Toolbar SideDrawer, Backdrop, <Link to="/add">Add</Link>
+        </div>
+        {/* {props.children} */}
+        <Route path="/add" exact component={AddTransfer} />
+      </Aux>
+    </BrowserRouter>
   );
 };
 
