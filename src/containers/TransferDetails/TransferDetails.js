cbaae8afbf3a847c307bda8ccebe12009ef0c5c6
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2020-09-10 22:58:40 +0200

    extract params from the route

--- src/containers/TransferDetails/TransferDetails.js
+++ src/containers/TransferDetails/TransferDetails.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-const TransferDetails = ({ categorie }) => {
+const TransferDetails = (props) => {
+  const categorie = props.match.params.cat;
+  const [amount, setAmount] = useState("");
+  const [title, setTitle] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div>
-      <h3>{categorie}</h3>
+      <h4>{categorie}</h4>
+      <h4>{title}</h4>
+      <h4>{amount}</h4>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="title"
+          placeholder="Title"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+        <input
+          type="number"
+          id="amount"
+          placeholder="Amount"
+          onChange={(e) => setAmount(e.target.value)}
+          value={amount}
+        />
+      </form>
     </div>
   );
 };
