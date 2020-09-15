e8bb2724e18b044bbbc7a14667bf2b2cc3aed767
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2020-09-15 12:26:41 +0200

    CategoriesList component removed

--- src/containers/AddTransfer/AddTransfer.js
+++ src/containers/AddTransfer/AddTransfer.js
@@ -1,7 +1,8 @@
 import React from "react";
-import Aux from "../../components/wraper";
-import CategoriesList from "../../components/CategoriesList";
 import classes from "../../App.module.scss";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 function AddTransfer() {
   const categoriesList = [
@@ -13,12 +14,30 @@ function AddTransfer() {
     "Rachunki - gaz",
     "Rachunki - prąd",
   ];
+  const Categories = categoriesList.map((categorie) => {
+    return (
+      <Link
+        to={{
+          pathname: `/add/details/${categorie}`,
+        }}
+        key={categorie}
+        className={classes.categoriesList__link}
+      >
+        <li className={classes.listItem}>
+          {categorie}
+          <FontAwesomeIcon icon={faChevronRight} color="#bcb8b1" />
+        </li>
+      </Link>
+    );
+  });
   return (
-    <Aux>
-      <form className={classes.addTransferForm}>
-        <CategoriesList categoriesList={categoriesList} />
-      </form>
-    </Aux>
+    <div className={classes.categories}>
+      <h4 className={classes.categories__clickOn}>Categories</h4>
+      <ul className={classes.categoriesList}>
+        {/* <CategoriesList categoriesList={categoriesList} /> */}
+        {Categories}
+      </ul>
+    </div>
   );
 }
 
