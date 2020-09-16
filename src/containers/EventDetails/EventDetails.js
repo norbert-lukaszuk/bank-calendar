67efe8029916601e5004a195f29a83fef81fc0d9
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2020-09-16 22:10:55 +0200

    defaultValue bug in input type date fixed

--- src/containers/EventDetails/EventDetails.js
+++ src/containers/EventDetails/EventDetails.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import classes from "../../App.module.scss";
 const EventDetails = (props) => {
   const gapi = window.gapi;
   const [event, setEvent] = useState();
@@ -6,7 +7,7 @@ const EventDetails = (props) => {
   const [amount, setAmount] = useState(0);
   const [eventId, setEventId] = useState("");
   const [date, setDate] = useState("");
-  console.log("EventDetails -> date", date);
+  console.log("EventDetails -> event", event);
   // get the single event from calendar
   const getEvent = () => {
     gapi.client.calendar.events
@@ -21,43 +22,44 @@ const EventDetails = (props) => {
         setTitle(resp.result.description.slice(sliceFrom + 1));
         setAmount(parseFloat(resp.result.description.slice(0, sliceTo)));
         setEventId(resp.result.id);
-        setDate(resp.result.start.dateTime);
+        setDate(new Date(resp.result.start.dateTime));
       });
   };
   // handle submit
   const handleSubmit = () => {};
   useEffect(getEvent, []);
   if (event) {
-    console.log(event.result);
+    console.log(event.result.start.dateTime);
     // setTitle(event.result.summary);
-    const hours = event.result.reminders.overrides[1].minutes / 60;
     return (
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          id="title"
-          value={title}
-          onChange={(e) => {
-            setTitle(e.target.value);
-          }}
-        />
-        <input
-          type="number"
-          id="amount"
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
-        />
-        <input
-          type="date"
-          id="date"
-          defaultValue={date}
-          onChange={(e) => setDate(e.target.value)}
-        />
-        <p>{event.result.summary}</p>
-        <p>{event.result.start.dateTime.slice(0, 10)}</p>
-        <p>Reminder {hours} houres before</p>
-        <button type="submit">Update</button>
-      </form>
+      <div className={classes.transferDetails}>
+        <h2>{event.result.summary}</h2>
+        <form onSubmit={handleSubmit} className={classes.transferDetails__form}>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
+          <input
+            type="number"
+            id="amount"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+          <input
+            type="date"
+            id="date"
+            defaultValue={event.result.start.dateTime.slice(0, 10)}
+            onChange={(e) => setDate(e.target.value)}
+          />
+          <p>{event.result.summary}</p>
+          <p>{event.result.start.dateTime.slice(0, 10)}</p>
+          <button type="submit">Update</button>
+        </form>
+      </div>
     );
   } else {
     return <h3>Loading...</h3>;
