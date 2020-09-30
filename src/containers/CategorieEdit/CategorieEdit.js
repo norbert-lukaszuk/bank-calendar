6cc6bbf556ba93d83123b6302c3c6da34597229d
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2020-09-30 13:20:42 +0200

    error while changing account number in state

--- src/containers/CategorieEdit/CategorieEdit.js
+++ src/containers/CategorieEdit/CategorieEdit.js
@@ -3,28 +3,41 @@ import classes from "../../App.module.scss";
 const db = window.firebase.firestore();
 
 function CategorieEdit(props) {
-  const [editedCategorie, setEditedCategorie] = useState({});
-  const [categorieName, setCategorieName] = useState(
-    editedCategorie.categorieName
-  );
-  const [bankDefault, setBankDefault] = useState(editedCategorie.bankDefault);
-  const [titlePrefill, setTitlePrefill] = useState(
-    editedCategorie.titlePrefill
-  );
-  const [accountNumber, setAccountNumber] = useState(
-    editedCategorie.bankAccount
-  );
-  console.log(editedCategorie);
-  const handleFormSubmit = () => {};
+  // const [editedCategorie, setEditedCategorie] = useState(null);
+  const [categorieName, setCategorieName] = useState("");
+  const [bankDefault, setBankDefault] = useState("");
+  const [bankName, setBankName] = useState([]);
+  const [titlePrefill, setTitlePrefill] = useState("");
+  const [accountNumber, setAccountNumber] = useState("");
+  console.log(accountNumber);
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    db.collection("categoriesReact")
+      .doc(props.match.params.id)
+      .update({
+        bankAccount: {
+          [bankDefault]: accountNumber,
+        },
+      })
+      .catch((err) => console.error(err));
+  };
   const getCategorieToEdit = () => {
     db.collection("categoriesReact")
       .doc(props.match.params.id)
       .get()
       .then((resp) => resp.data())
-      .then((data) => setEditedCategorie(data))
+      .then((data) => {
+        setCategorieName(data.categorieName);
+        setBankName(data.bankName);
+        setBankDefault(data.bankDefault);
+        setTitlePrefill(data.titlePrefill);
+        setAccountNumber(data.bankAccount);
+      })
       .catch((err) => console.log(err));
   };
+
   useEffect(getCategorieToEdit, []);
+
   return (
     <div className={classes.newCategorie}>
       <form onSubmit={handleFormSubmit} className={classes.newCategorie__form}>
@@ -68,20 +81,22 @@ function CategorieEdit(props) {
           onChange={(e) => setTitlePrefill(e.target.value)}
           value={titlePrefill}
         />
-        {/* {editedCategorie
-          ? editedCategorie.bankName.map((e) => {
-              return <p>{e}</p>;
-            })
-          : null} */}
+        {/* {bankName.map((e) => {
+          return <p>{e}</p>;
+        })} */}
         <label htmlFor="accountNumber">Account number</label>
         <input
           type="text"
           id="accountNumber"
-          onChange={(e) => setAccountNumber(e.target.value)}
-          value={accountNumber}
+          onChange={(e) =>
+            setAccountNumber(
+              (prevState) => (prevState[bankDefault] = e.target.value)
+            )
+          }
+          value={accountNumber[bankDefault]}
         />
         <button type="submit" className={classes.newCategorie__submitButton}>
-          Submit
+          Submit change
         </button>
       </form>
     </div>
